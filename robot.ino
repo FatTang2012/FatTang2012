23f5dd6ae9ef4e16f201f38303773d3992bfb660
QuanCoBap <142882761+QuanCoBap@users.noreply.github.com>
2023-10-31 21:43:34 +0700

    Add files via upload

--- robot.ino
+++ robot.ino
@@ -20,9 +20,21 @@ Stepper myStepper2(stepsPerRevolution, stepPins2[0], stepPins2[1], stepPins2[2],
 #define ENA 10
 #define ENB 11
 
-//cambienvatcan
-const int trig = 2;     // chân trig của HC-SR04
-const int echo = 3;     // chân echo của HC-SR04
+
+
+
+//cambienvatcantrai
+const int trig_trai = 2;     // chân trig của HC-SR04
+const int echo_trai = 3;     // chân echo của HC-SR04
+
+//cambienvatcangiua
+const int trig_giua = 4;     // chân trig của HC-SR04
+const int echo_giua = 5;     // chân echo của HC-SR04
+
+//cambienvatcanphai
+const int trig_phai = 6;     // chân trig của HC-SR04
+const int echo_phai = 7;     // chân echo của HC-SR04
+
 
 void setup()
 {
@@ -47,10 +59,20 @@ Serial.begin(9600);
 analogWrite(ENA, 120);
 analogWrite(ENB, 120);//đi chậm cho khỏi trượt Line
 
-//cambienvatcan
+//cambienvatcantrai
     Serial.begin(9600);     // giao tiếp Serial với baudrate 9600
-    pinMode(trig,OUTPUT);   // chân trig sẽ phát tín hiệu
-    pinMode(echo,INPUT);    // chân echo sẽ nhận tín hiệu
+    pinMode(trig_trai,OUTPUT);   // chân trig sẽ phát tín hiệu
+    pinMode(echo_trai,INPUT);    // chân echo sẽ nhận tín hiệu
+
+    //cambienvatcangiua
+    Serial.begin(9600);     
+    pinMode(trig_giua,OUTPUT);   
+    pinMode(echo_giua,INPUT);    
+
+    //cambienvatcanphai
+    Serial.begin(9600);    
+    pinMode(trig_phai,OUTPUT);   
+    pinMode(echo_phai,INPUT);    
 }
 
 void loop()
@@ -73,8 +95,7 @@ if (line_sau >=1){dithang();}//đụng line sau thì lùi lại
 
   //camrbien hc-sr04
   //cambientrai  
-     cambien()
-    if(distance < 5)
+    if(cambientrai() < 5)
     {
      void xichphai();
     }
@@ -82,10 +103,7 @@ if (line_sau >=1){dithang();}//đụng line sau thì lùi lại
 
     
     
-    /* In kết quả ra Serial Monitor */
-    Serial.print(distance);
-    Serial.println("cm");
-    delay(200);
+    
 
 
                                           }
@@ -115,7 +133,7 @@ void quayphai(){
            digitalWrite(inB2,LOW);
 }
 void xichtrai(){
-      analogWrite(ENA, OUTPUT);
+      analogWrite(ENA, 120);
       digitalWrite(inA1,HIGH);
       digitalWrite(inA2,LOW);
       digitalWrite(inB1,HIGH);
@@ -124,25 +142,74 @@ void xichtrai(){
 void xichphai(){ 
       digitalWrite(inA1,HIGH);
       digitalWrite(inA2,LOW);
-      analogWrite(ENB, OUTPUT);
+      analogWrite(ENB, 120);
       digitalWrite(inB1,HIGH);
       digitalWrite(inB2,LOW);
 }
+
 //camrbien hc-sr04
-int cambien(){
+float cambientrai(){
     unsigned long duration; // biến đo thời gian
-    int distance; 
+    int distance1; 
     /* Phát xung từ chân trig */
-    digitalWrite(trig,0);   // tắt chân trig
+    digitalWrite(trig_trai,0);   // tắt chân trig
     delayMicroseconds(2);
-    digitalWrite(trig,1);   // phát xung từ chân trig
+    digitalWrite(trig_trai,1);   // phát xung từ chân trig
     delayMicroseconds(5);   // xung có độ dài 5 microSeconds
-    digitalWrite(trig,0);   // tắt chân trig
+    digitalWrite(trig_trai,0);   // tắt chân trig
     
     /* Tính toán thời gian */
     // Đo độ rộng xung HIGH ở chân echo. 
-    duration = pulseIn(echo,HIGH);  
+    duration = pulseIn(echo_trai,HIGH);  
     // Tính khoảng cách đến vật.
-    distance = int(duration/2/29.412);
+    distance1 = int(duration/2/29.412);
+    /* In kết quả ra Serial Monitor */
+    Serial.print(distance1);
+    Serial.println("cm");
+    delay(200);
+    return distance1;
 }
 
+float cambiengiua(){
+    unsigned long duration; // biến đo thời gian
+    int distance2; 
+    /* Phát xung từ chân trig */
+    digitalWrite(trig_giua,0);   // tắt chân trig
+    delayMicroseconds(2);
+    digitalWrite(trig_giua,1);   // phát xung từ chân trig
+    delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+    digitalWrite(trig_giua,0);   // tắt chân trig
+    
+    /* Tính toán thời gian */
+    // Đo độ rộng xung HIGH ở chân echo. 
+    duration = pulseIn(echo_giua,HIGH);  
+    // Tính khoảng cách đến vật.
+    distance2 = int(duration/2/29.412);
+    /* In kết quả ra Serial Monitor */
+    Serial.print(distance2);
+    Serial.println("cm");
+    delay(200);
+    return distance2;
+}
+
+float cambienphai(){
+    unsigned long duration; // biến đo thời gian
+    int distance3; 
+    /* Phát xung từ chân trig */
+    digitalWrite(trig_phai,0);   // tắt chân trig
+    delayMicroseconds(2);
+    digitalWrite(trig_phai,1);   // phát xung từ chân trig
+    delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+    digitalWrite(trig_phai,0);   // tắt chân trig
+    
+    /* Tính toán thời gian */
+    // Đo độ rộng xung HIGH ở chân echo. 
+    duration = pulseIn(echo_giua,HIGH);  
+    // Tính khoảng cách đến vật.
+    distance3 = int(duration/2/29.412);
+    /* In kết quả ra Serial Monitor */
+    Serial.print(distance3);
+    Serial.println("cm");
+    delay(200);
+    return distance3;
+}
